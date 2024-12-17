989807b6f9f479718f1e4eadf4606b6b111c1740
SamuelJanas <sneakyinsect@gmail.com>
2024-12-17 09:06:44 +0100

    center content on the first slide

--- dashboards/raw_data.py
+++ dashboards/raw_data.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 
 def main():
-    with st.echo():
-        raw_data = pd.read_csv('data/witcher.csv', index_col=0)
+    _, cen, _ = st.columns([1, 3, 1])
+    with cen: # Center the content
+        with st.echo():
+            raw_data = pd.read_csv('data/witcher.csv', index_col=0)
 
-        st.markdown("## Raw dataset")
-        st.write(raw_data)
+            st.markdown("## Raw dataset")
+            st.write(raw_data)
